950a30eeaf58b1b1f060e96f10975dd92226ae85
Meng Feng <mfeng.pi@gmail.com>
2024-03-12 19:45:53 -0400

    finalize reset with and without constraints

--- pud/envs/safe_pointenv/safe_wrappers.py
+++ pud/envs/safe_pointenv/safe_wrappers.py
@@ -7,6 +7,14 @@ from typing import Union
 class SafeGoalConditionedPointWrapper(gym.Wrapper):
     """Wrapper that appends goal to observation produced by environment.
     Sample with safety constraints
+
+    Option 1: sample goals of any distance subject to step cost == 0
+        Potential long distances, but there exists viable solutions
+    Option 2:  distance constraints subject to step cost == 0
+        Distance constraints, but there exists viable solutions
+    Option 3: distance constraints subject to 0< step cost < cost limit
+        perhaps only good for training
+        Distance constraints, but there may not exist viable solutions, but trajectories whose step cost < cost limit 
     """
 
     def __init__(self, 
@@ -33,6 +41,7 @@ class SafeGoalConditionedPointWrapper(gym.Wrapper):
         self._prob_constraint = prob_constraint
         self._min_dist = min_dist
         self._max_dist = max_dist
+        self._sample_key = "ub"
         super(SafeGoalConditionedPointWrapper, self).__init__(env)
         self.observation_space = gym.spaces.Dict({
             'observation': env.observation_space,
@@ -53,9 +62,6 @@ class SafeGoalConditionedPointWrapper(gym.Wrapper):
         inds_cands = np.where(mask_cands)
         self.start_n_goal_candidates["lb"] = np.column_stack(inds_cands) # x1, y1, x2, y2
 
-        self.sample_key = "ub"
-        
-
         #self.safe_empty_states_ub = self.env.gather_safe_empty_states(cost_limit=0.0)
         #self.safe_empty_states_lb = self.env.gather_safe_empty_states(cost_limit=self.env.cost_limit)
 
@@ -63,7 +69,8 @@ class SafeGoalConditionedPointWrapper(gym.Wrapper):
 
     def sample_start_n_goal(self, key:Union[tuple, str]="ub"):
         """sample start and goal without distance constraint, 
-        safe: viable solutions are guaranteed to exist
+        "ub": viable solutions are guaranteed to exist
+        "lb": viable solutions are NOT guarantee to exist
         """
         ind = np.random.randint(len(self.start_n_goal_candidates[key]))
         self.start_n_goal_candidates[key][ind]
@@ -80,7 +87,7 @@ class SafeGoalConditionedPointWrapper(gym.Wrapper):
         sampling start and goal states with guaranteed solution
         with distance cosntraints
         """
-        cand_key = (min_dist, max_dist)
+        cand_key = (key, min_dist, max_dist)
         if not (cand_key in self.start_n_goal_candidates.keys()):
             mask_lb = self.env._safe_apsp[key] > min_dist
             mask_ub = self.env._safe_apsp[key] < max_dist
@@ -98,9 +105,15 @@ class SafeGoalConditionedPointWrapper(gym.Wrapper):
         ])
 
     def reset(self):
-        output = self.sample_start_n_goal(key=self.sample_key)
-        self._goal = output["sg"]
-        obs = output["s0"]
+        out = None
+        if np.random.random() < self._prob_constraint:
+            out = self.sample_safe_start_n_goal_in_dists(self._min_dist, self._max_dist, key = self._sample_key)
+        else:
+            out = self.sample_start_n_goal(key=self._sample_key)
+        
+        self._goal = out["sg"]
+        obs = out["s0"]
+        self.state = obs.copy()
         cost = self.env.get_state_cost(self._goal)
         return {'observation': self._normalize_obs(obs),
                 'goal': self._normalize_obs(self._goal), 
@@ -108,14 +121,18 @@ class SafeGoalConditionedPointWrapper(gym.Wrapper):
                 }
 
     def step(self, action):
-        obs, _, _, _ = self.env.step(action)
+        obs, _, _, info = self.env.step(action)
         rew = -1.0
         done = self._is_done(obs, self._goal)
         return {'observation': self._normalize_obs(obs),
-                'goal': self._normalize_obs(self._goal)}, rew, done, {}
+                'goal': self._normalize_obs(self._goal)}, rew, done, info
 
-    def set_sample_goal_args(self, prob_constraint=None,
-                             min_dist=None, max_dist=None):
+    def set_sample_goal_args(self, 
+            prob_constraint=None,
+            min_dist=None, 
+            max_dist=None,
+            sample_key="ub",
+            ):
         assert prob_constraint is not None
         assert min_dist is not None
         assert max_dist is not None
@@ -124,63 +141,13 @@ class SafeGoalConditionedPointWrapper(gym.Wrapper):
         self._prob_constraint = prob_constraint
         self._min_dist = min_dist
         self._max_dist = max_dist
+        self._sample_key = sample_key
 
     def _is_done(self, obs, goal):
         """Determines whether observation equals goal."""
         return np.linalg.norm(obs - goal) < self._threshold_distance
 
-    def _sample_goal(self, obs):
-        """Sampled a goal observation.
-        Option 1: sample goals of any distance subject to step cost == 0
-            Potential long distances, but there exists viable solutions
-        Option 2:  distance constraints subject to step cost == 0
-            Distance constraints, but there exists viable solutions
-        Option 3: distance constraints subject to 0< step cost < cost limit
-            Distance constraints, but there may not exist viable solutions, but trajectories whose step cost < cost limit 
-        """
-        return self._sample_safe_goal_constrained(obs, self._min_dist, self._max_dist)
-
-    def _sample_safe_goal_constrained(self, obs, min_dist, max_dist):
-        """Samples a goal with dist min_dist <= d(observation, goal) <= max_dist.
-
-        Args:
-          obs: observation (without goal).
-          min_dist: (int) minimum distance to goal.
-          max_dist: (int) maximum distance to goal.
-        Returns:
-          observation: observation (without goal).
-          goal: a goal observation.
-        """
-        (i, j) = self.env._discretize_state(obs)
-        mask = np.logical_and(self.env._safe_apsp["ub"][i, j] >= min_dist,
-                              self.env._safe_apsp["ub"][i, j] <= max_dist)
-        mask = np.logical_and(mask, self.env._walls == 0)
-        candidate_states = np.where(mask)
-        num_candidate_states = len(candidate_states[0])
-        if num_candidate_states == 0:
-            return (obs, None)
-        goal_index = np.random.choice(num_candidate_states)
-        goal = np.array([candidate_states[0][goal_index],
-                         candidate_states[1][goal_index]],
-                        dtype=np.float32)
-        goal += np.random.uniform(size=2)
-        dist_to_goal = self.env._get_distance(obs, goal)
-        assert min_dist <= dist_to_goal <= max_dist
-        assert not self.env._is_blocked(goal)
-        return (obs, goal)
-
-    def _sample_safe_goal_unconstrained(self, obs):
-        """Samples a goal without any constraints.
-
-        Args:
-          obs: observation (without goal).
-        Returns:
-          observation: observation (without goal).
-          goal: a goal observation.
-        """
-        pass
-
     @property
     def max_goal_dist(self):
-        apsp = self.env._apsp
+        apsp = self.env._safe_apsp["ub"]
         return np.max(apsp[np.isfinite(apsp)])
--- pud/envs/safe_pointenv/unit_tests/test_safe_wrapper.py
+++ pud/envs/safe_pointenv/unit_tests/test_safe_wrapper.py
@@ -4,7 +4,7 @@ from pud.envs.safe_pointenv.safe_wrappers import SafeGoalConditionedPointWrapper
 import numpy as np
 
 """
-python pud/envs/safe_pointenv/unit_tests/test_safe_wrapper.py TestSafeWrapper.test_sample_safe_start_n_goal_in_dists
+python pud/envs/safe_pointenv/unit_tests/test_safe_wrapper.py TestSafeWrapper.test_step
 """
 
 class TestSafeWrapper(unittest.TestCase):
@@ -56,10 +56,36 @@ class TestSafeWrapper(unittest.TestCase):
             self.assertTrue(self.p_env._safe_apsp["ub"][key[0],key[1],key[2],key[3]] > dist_limits[0])
             self.assertTrue(self.p_env._safe_apsp["ub"][key[0],key[1],key[2],key[3]] < dist_limits[1])
 
-    
-    
-    def test_reset(self):
+    def test_reset_no_constraint(self):
+        for _ in range(100):
+            out = self.w_env.reset()
+            self.assertTrue(
+                self.w_env.get_state_cost(out["observation"]) <= 0.0
+            )
+            self.assertTrue(
+                self.w_env.get_state_cost(out["goal"]) <= 0.0
+            )
+
+    def test_reset_with_constraint(self):
+        self.w_env.set_sample_goal_args(
+            prob_constraint=1.0,
+            min_dist=2,
+            max_dist=10,
+            sample_key="ub",
+        )
+        for _ in range(100):
+            out = self.w_env.reset()
+            self.assertTrue(
+                self.w_env.get_state_cost(out["observation"]) <= 0.0
+            )
+            self.assertTrue(
+                self.w_env.get_state_cost(out["goal"]) <= 0.0
+            )
+
+    def test_step(self):
         self.w_env.reset()
+        at = self.w_env.action_space.sample()
+        next_state, reward, done, info = self.w_env.step(at)
 
 if __name__ == '__main__':
     unittest.main()
