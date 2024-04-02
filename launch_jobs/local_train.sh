3ad5918919211c3cead132ac673bf2cc645a9398
Meng Feng <mfeng.pi@gmail.com>
2024-04-02 17:08:02 -0400

    good state: training script update

--- launch_jobs/local_train.sh
+++ launch_jobs/local_train.sh
@@ -1,1 +1,10 @@
-python pud/algos/train_PointEnv.py --cfg configs/config_SafePointEnv.yaml
+# note: must have empty space between xx: [ xx ]
+# -z tests if condition true, -n no tests if condition if false
+mode=$1
+if [ -z $mode ]; then
+    echo "Debugging mode"
+    python pud/algos/train_PointEnv.py --cfg configs/config_SafePointEnv_debug.yaml
+else
+    echo "Normal Mode"
+    python pud/algos/train_PointEnv.py --cfg configs/config_SafePointEnv.yaml
+fi
