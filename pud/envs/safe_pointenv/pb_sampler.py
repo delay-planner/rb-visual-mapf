1263821583ba642a0a29ba03b0f1d6ae18206192
Meng Feng <mfeng.pi@gmail.com>
2024-04-18 16:22:31 -0400

    refactor agg

--- pud/envs/safe_pointenv/pb_sampler.py
+++ pud/envs/safe_pointenv/pb_sampler.py
@@ -2,16 +2,29 @@ from typing import List, Union
 
 import numpy as np
 
-from pud.algos.data_struct import arg_topk, get_nd_inds_set
+from pud.algos.data_struct import arg_topk, get_nd_inds_set, arg_group_vals
 from pud.algos.lagrange.drl_ddpg_lag import DRLDDPGLag
 from pud.envs.safe_pointenv.safe_wrappers import \
     SafeGoalConditionedPointWrapper
 
 
-def max_pairwise_cost(agent:DRLDDPGLag, rb_vec:np.ndarray):
+def calc_pairwise_cost(agent:DRLDDPGLag, rb_vec:np.ndarray, ensemble_agg="max"):
     pcost = agent.get_pairwise_cost(rb_vec, aggregate=None)
-    pcost_max = np.max(pcost, axis=0)
-    return pcost_max
+    pcost_agg = None
+    if ensemble_agg == "max":
+        pcost_agg = np.max(pcost, axis=0)
+    elif ensemble_agg == "mean":
+        pcost_agg = np.mean(pcost, axis=0)
+    return pcost_agg
+
+def calc_pairwise_dist(agent:DRLDDPGLag, rb_vec:np.ndarray, ensemble_agg="max"):
+    pdist = agent.get_pairwise_dist(rb_vec, aggregate=None)
+    pdist_agg = None
+    if ensemble_agg == "max":
+        pdist_agg = np.max(pdist, axis=0)
+    elif ensemble_agg == "mean":
+        pdist_agg = np.mean(pdist, axis=0)
+    return pdist_agg
 
 def sample_pbs_by_agent(
         env:SafeGoalConditionedPointWrapper, 
@@ -19,6 +32,7 @@ def sample_pbs_by_agent(
         num_states:int=100,
         target_val:List[float]=None,
         pval_f = None, # function to generate pairwise values (e.g., dists, cost, ...) 
+        ensemble_agg:str="max",
         K:int=5, # num of samples nearest to the target metric
         ) -> List[dict]:
     """sample problems with target metrics according to the predictions of the agent
@@ -37,12 +51,10 @@ def sample_pbs_by_agent(
     rb_vec = np.array([x["observation"] for x in rb_vec])
 
     ## predict the pairwise costs
-    pvals = pval_f(agent, rb_vec) # num_states x num_states
-
+    pvals = pval_f(agent, rb_vec, ensemble_agg) # num_states x num_states
     diff = np.abs(pvals - target_val)
     inds = arg_topk(-diff, topK=K) # find K minimum entries
     #inds_set = get_nd_inds_set(inds)
-
     nearest_pbs = [None] * K
     for n in range(K):
         i,j = inds[0][n], inds[1][n]
