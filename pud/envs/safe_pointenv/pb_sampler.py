c46cd7dd0de5f00c091f0a4c17cfd39628fe7bb3
Meng Feng <mfeng.pi@gmail.com>
2024-04-19 16:36:30 -0400

    sample cost probs with dist constraints

--- pud/envs/safe_pointenv/pb_sampler.py
+++ pud/envs/safe_pointenv/pb_sampler.py
@@ -64,3 +64,54 @@ def sample_pbs_by_agent(
             "info": {"prediction": pvals[i,j]},
         }
     return nearest_pbs
+
+def sample_cost_pbs_by_agent(
+        env:SafeGoalConditionedPointWrapper, 
+        agent:DRLDDPGLag, 
+        num_states:int=100,
+        target_val:List[float]=None,
+        min_dist: float = 0,
+        max_dist: float = 10,
+        ensemble_agg:str="mean",
+        K:int=5, # num of samples nearest to the target metric
+    ):
+    """
+    filter based on distance constraints
+    problems whose start and goals are seperated too far away is 
+    meaningless as they are handled by the HRL
+    if failed, return an empty list, because the test results would not be informative
+    
+    """
+    rb_vec = [None] * num_states
+    for i in range(num_states):
+        s0, info = env.reset_orig()
+        rb_vec[i] = s0
+    rb_vec = np.array([x["observation"] for x in rb_vec])
+
+    pdists = calc_pairwise_dist(agent, rb_vec, ensemble_agg) # num_states x num_states
+    # filter based on distance constraints
+    lb_mask = pdists >= min_dist
+    ub_mask = pdists <= max_dist
+    prod_mask = lb_mask * ub_mask
+    
+    gInds = np.where(prod_mask)
+    if len(gInds[0]) == 0:
+        return []
+    else:    
+        pcosts = calc_pairwise_cost(agent, rb_vec, ensemble_agg) # num_states x num_states
+        pcosts_gInds = pcosts[gInds]
+        diff = np.abs(pcosts_gInds - target_val)
+        mInds = arg_topk(-diff, topK=K) # find K minimum entries
+        gmInds = (gInds[0][mInds], gInds[1][mInds])
+
+        nearest_pbs = [None] * K
+        for n in range(K):
+            i,j = gmInds[0][n], gmInds[1][n]
+            nearest_pbs[n] = {
+                "start": env.de_normalize_obs(rb_vec[i]),
+                "goal": env.de_normalize_obs(rb_vec[j]),
+                "info": {"prediction": pcosts[i,j],
+                        "proj_dist": pdists[i,j]
+                        },
+                    }
+        return nearest_pbs
