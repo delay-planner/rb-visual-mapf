78ddb4c1db3ca735147d8346f0d12c1562326173
Meng Feng <mfeng.pi@gmail.com>
2024-04-20 18:49:40 -0400

    add terminal training arg

--- launch_jobs/local_train.sh
+++ launch_jobs/local_train.sh
@@ -1,9 +1,11 @@
 # !/bin/sh
 
-comment="self_train_eval"
+env="FourRooms"
+
+comment="self_train_eval_with_lag"
 SLURM_JOB_ID=local
 experiment_dir="runs/results"
-log_dir=${experiment_dir}/job_${SLURM_JOB_ID}_${comment}
+log_dir=${experiment_dir}/${env}/job_${SLURM_JOB_ID}_${comment}
 
 echo "project root directory: ${project_root}"
 echo "experiment directory: ${log_dir}"
@@ -13,9 +15,13 @@ config="configs/config_PointEnv_Queue.yaml"
 #config="configs/config_PointEnv_Queue_debug.yaml"
 #config="configs/config_SafePointEnv_debug.yaml"
 
+device="cpu"
+#device="cuda:0"
+
 cd "${project_root}"
 python pud/algos/train_PointEnv.py \
     --cfg $config \
+    --env $env \
     --logdir ${log_dir} \
-    --device cuda:0 \
+    --device ${device} \
     --pbar
