ce20377ecf8346b5970ff0261faa41a2cfe6041d
Meng Feng <mfeng.pi@gmail.com>
2024-04-29 16:25:50 -0400

    add python debugger runner

--- launch_jobs/local_train.sh
+++ launch_jobs/local_train.sh
@@ -3,7 +3,7 @@
 #env="FourRooms"
 env="CentralObstacle"
 
-comment="rev_pb_sampler"
+comment="rev_pb_sampler_max_2_debug"
 SLURM_JOB_ID=local
 experiment_dir="runs/results"
 log_dir=${experiment_dir}/${env}/job_${SLURM_JOB_ID}_${comment}
@@ -20,10 +20,33 @@ config="configs/config_PointEnv_Queue.yaml"
 device="cuda:0"
 
 cd "${project_root}"
-python pud/algos/train_PointEnv.py \
-    --cfg $config \
-    --env $env \
-    --logdir ${log_dir} \
-    --device ${device} \
-    --visual \
-    --pbar
+
+debug=true
+
+# note: must have empty space between xx: [ xx ]
+# -z tests if condition true, -n no tests if condition if false
+if [[ -n ${debug} ]]; then
+    echo "[INFO] running in debug mode"
+    echo "[Action Needed] need to launch the debugger client"
+    python -m debugpy \
+        --listen localhost:5678 \
+        --wait-for-client \
+        pud/algos/train_PointEnv.py \
+        --cfg $config \
+        --env $env \
+        --logdir ${log_dir} \
+        --device ${device} \
+        --visual \
+        --pbar
+else
+    echo "[INFO] running in normal mode"
+    python pud/algos/train_PointEnv.py \
+        --cfg $config \
+        --env $env \
+        --logdir ${log_dir} \
+        --device ${device} \
+        --visual \
+        --pbar
+fi
+
+
