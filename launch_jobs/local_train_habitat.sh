4c12a8152b6a833d2fcee54ba30b3cc3adac9426
Meng Feng <mfeng.pi@gmail.com>
2024-05-10 02:46:16 -0400

    more args

--- launch_jobs/local_train_habitat.sh
+++ launch_jobs/local_train_habitat.sh
@@ -9,7 +9,8 @@ log_dir=${experiment_dir}/${env}/job_${SLURM_JOB_ID}_${comment}
 echo "project root directory: ${project_root}"
 echo "experiment directory: ${log_dir}"
 
-config=configs/config_SafeHabitatEnv.yaml
+#config=configs/config_SafeHabitatEnv.yaml
+config=configs/config_SafeHabitatEnv_Queue_debug.yaml
 device="cpu"
 #device="cuda:0"
 
@@ -22,6 +23,8 @@ debugger_port=5678
 
 cost_name="linear"
 cost_radius=10.0
+scene=scene_datasets/habitat-test-scenes/skokloster-castle.glb
+apsp_path=pud/envs/safe_habitatenv/apsps/skokloster/apsp.pickle
 
 # note: must have empty space between xx: [ xx ]
 # -z tests if condition true, -n no tests if condition if false
@@ -32,7 +35,11 @@ if [[ -n ${debugger_port} ]]; then
         --listen localhost:${debugger_port} \
         --wait-for-client \
         pud/algos/train_HabitatEnv.py \
+        --scene $scene \
+        --apsp_path $apsp_path \
         --cfg $config \
+        --cost_name $cost_name \
+        --cost_radius $cost_radius \
         --logdir ${log_dir} \
         --device ${device} \
         --visual \
@@ -40,6 +47,8 @@ if [[ -n ${debugger_port} ]]; then
 else
     echo "[INFO] running in normal mode"
     python pud/algos/train_HabitatEnv.py \
+        --scene $scene \
+        --apsp_path $apsp_path \
         --cfg $config \
         --cost_name $cost_name \
         --cost_radius $cost_radius \
