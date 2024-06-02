975de75cba74071f13cc0916713252d371cde86e
Meng Feng <mfeng.pi@gmail.com>
2024-06-02 12:08:13 -0400

    pretrain

--- pud/pretrain_job.sh
+++ pud/pretrain_job.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+emb_dim=128
+train_itrs=100000
+train_batch_size=32
+logdir=runs/pretrain/emb_${emb_dim}
+device="cuda:0"
+
 python pud/pretrain_encoder.py \
-    --logdir runs/pretrain/test \
-    --device cuda:0
+    --logdir $logdir \
+    --device $device \
+    --train_itrs $train_itrs \
+    --train_batch_size $train_batch_size \
+    --emb_dim $emb_dim
