86151ba4493fd025b16dda790f34b67957bc2175
Meng Feng <mfeng.pi@gmail.com>
2024-06-30 19:42:47 -0700

    add encoder implemenation

--- pud/visual_models.py
+++ pud/visual_models.py
@@ -9,7 +9,80 @@ from pud.ddpg import UVFDDPG, Actor, Critic
 from termcolor import colored
 
 
-class VisualEncoder(nn.Module):
+class Encoder (nn.Module):
+    """
+    Encoder parent class, must have goal-conditioned get_latent_state
+    """
+    def __init__(self, **kwargs):
+        super(Encoder, self).__init__()
+
+    def get_latent_state(self, 
+            image_state:Union[torch.Tensor, dict, np.ndarray],
+            device:torch.device):
+        """convert image state to latent state"""
+        # move input to torch device
+        inp = inp_to_torch_device(image_state, device=device)
+
+        if torch.is_tensor(inp):
+            return self.forward(inp)
+        elif isinstance(inp, dict):
+            if len(inp["observation"].shape) == 5:
+                # batch is constructed on dim 0, 
+                # e.g., batch_size, 4, 64, 64, 4
+                # combine batch_size and 4 (first dim) for batch computation
+                batch_size, num_images, width, height, num_channels  = inp["observation"].shape
+                obs_cat = inp["observation"].reshape([batch_size*num_images, width, height, num_channels])
+                goal_cat = inp["goal"].reshape([batch_size*num_images, width, height, num_channels])
+                if False: # quick check, verify batch is matched after reshape
+                    for ii in range(batch_size):
+                        assert torch.allclose(obs_cat[ii*4:ii*4+4], inp["observation"][ii].squeeze())
+                inp["observation"] = obs_cat
+                inp["goal"] = goal_cat
+            latent_obs = self.forward(inp["observation"].float())
+            latent_goal = self.forward(inp["goal"].float())
+            latent_state = {}
+            for key in inp:
+                if key == "observation":
+                    latent_state[key] = latent_obs
+                elif key == "goal":
+                    latent_state[key] = latent_goal
+                else:
+                    latent_state[key] = inp[key]
+            return latent_state
+
+
+class VisualRGBEncoder (Encoder):
+    def __init__(self,
+            in_channels:int=4,
+            embedding_size:int=256,
+            width:int=32,
+            height:int=32,
+            act_fn=nn.SELU,
+            ):
+        super(VisualRGBEncoder, self).__init__()
+        self.conv_nets = [
+            nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=16, 
+                kernel_size=8, 
+                stride=4
+            ),  # 64x64 -> 15x15
+            nn.Conv2d(16, 32, kernel_size=4, stride=4),  # 15x15 -> 3x3
+        ]
+        self.act_fns = [
+            act_fn(),
+            act_fn(),
+        ]
+
+        self.mlp_nets = [
+            nn.Linear(embedding_size, embedding_size)
+        ]
+
+    def forward(self, inp):
+        pass
+
+
+class VisualEncoder(Encoder):
     """
     conv are performed on individual images (4 directions)
     -> image embeddings
@@ -57,40 +130,6 @@ class VisualEncoder(nn.Module):
         out = out.reshape(batch_size, -1)
         out = self.l1(out)
         return out
-    
-    def get_latent_state(self, 
-            image_state:Union[torch.Tensor, dict, np.ndarray],
-            device:torch.device):
-        """convert image state to latent state"""
-        # move input to torch device
-        inp = inp_to_torch_device(image_state, device=device)
-
-        if torch.is_tensor(inp):
-            return self.forward(inp)
-        elif isinstance(inp, dict):
-            if len(inp["observation"].shape) == 5:
-                # batch is constructed on dim 0, 
-                # e.g., batch_size, 4, 64, 64, 4
-                # combine batch_size and 4 (first dim) for batch computation
-                batch_size, num_images, width, height, num_channels  = inp["observation"].shape
-                obs_cat = inp["observation"].reshape([batch_size*num_images, width, height, num_channels])
-                goal_cat = inp["goal"].reshape([batch_size*num_images, width, height, num_channels])
-                if False: # quick check, verify batch is matched after reshape
-                    for ii in range(batch_size):
-                        assert torch.allclose(obs_cat[ii*4:ii*4+4], inp["observation"][ii].squeeze())
-                inp["observation"] = obs_cat
-                inp["goal"] = goal_cat
-            latent_obs = self.forward(inp["observation"].float())
-            latent_goal = self.forward(inp["goal"].float())
-            latent_state = {}
-            for key in inp:
-                if key == "observation":
-                    latent_state[key] = latent_obs
-                elif key == "goal":
-                    latent_state[key] = latent_goal
-                else:
-                    latent_state[key] = inp[key]
-            return latent_state
 
     def reset_parameters(self):
         for i in range(len(self.conv_net)):
@@ -98,6 +137,7 @@ class VisualEncoder(nn.Module):
                 torch.nn.init.xavier_uniform_(self.conv_net[i].weight)
         variance_initializer_(self.l1.weight, scale=1./3., mode='fan_in', distribution='uniform')
         torch.nn.init.zeros_(self.l1.bias)
+
 
 class VisualDecoder (nn.Module):
     def __init__(self, emb_size:int=256):
