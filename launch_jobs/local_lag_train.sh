798bbc61ad4d54bdbac8897dcdd8551d8e660630
Meng Feng <mfeng.pi@gmail.com>
2024-08-04 23:17:03 -0400

    train and illustration

--- launch_jobs/local_lag_train.sh
+++ launch_jobs/local_lag_train.sh
@@ -1,33 +1,20 @@
 # !/bin/sh
 
-#env="FourRooms"
-env="CentralObstacle"
-
 #device="cpu"
-device="cuda:0"
-
-#ckpt="runs/results/job_local_self_train_eval_with_lag/2024-04-20-18-31-00/ckpt/ckpt_0300000"
-#config="runs/results/job_local_self_train_eval_with_lag/2024-04-20-18-31-00/bk/bk_config.yaml"
-
-#ckpt="runs/results/CentralObstacle/job_local_rev_pb_sampler_max_2_debug/2024-04-29-16-42-03/ckpt/ckpt_0300000"
-#config="runs/results/CentralObstacle/job_local_rev_pb_sampler_max_2_debug/2024-04-29-16-42-03/bk/bk_config.yaml"
-
-ckpt="runs/results/CentralObstacle/job_local_cost_limit=20/2024-05-03-23-30-39/ckpt/ckpt_0300000"
-config="runs/results/CentralObstacle/job_local_cost_limit=20/2024-05-03-23-30-39/bk/bk_config.yaml"
+device="cuda:1"
 
 cd "${project_root}"
 
-#debugger_port=5679
-illustration_pb_file="pud/envs/safe_pointenv/illustration_set/CentralObstacle.txt"
+ckpt="runs_debug/results/CenterDot/job_530956.centerdot/2024-07-30-16-52-28/ckpt/ckpt_0293000"
+config="runs_debug/results/CenterDot/job_530956.centerdot/2024-07-30-16-52-28/bk/bk_config.yaml"
+lambda_lr=10
+illustration_pb_file="pud/envs/safe_pointenv/illustration_set/CenterDot_resize_3_linear_r3.txt"
+cost_limit=10.0
 
-#additional_comment="uniformly sample the training cost target at training problem generation, test set is isolated"
-
-lambda_lr=0.001
-cost_limit=5
 num_iterations=600000
 collect_steps=20
 
-#profile_output="runs/results/CentralObstacle/job_local_rev_pb_sampler_max_2_debug/2024-04-29-16-42-03/result.prof"
+#debugger_port=5678
 
 if [[ -n ${debugger_port} ]]; then
     echo "[INFO] running in debug mode"
@@ -43,8 +30,12 @@ if [[ -n ${debugger_port} ]]; then
             --cost_limit $cost_limit \
             --illustration_pb_file $illustration_pb_file \
             --lambda_lr $lambda_lr \
+            --num_iterations $num_iterations \
+            --eval_interval 10 \
+            --initial_collect_steps 10 \
             --visual \
-            --pbar
+            --pbar \
+            --verbose
 else
     echo "[INFO] running in normal mode"
     python pud/algos/train_lag_policy.py \
