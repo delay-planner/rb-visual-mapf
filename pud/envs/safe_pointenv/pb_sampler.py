aa0035c2bad69a1dad3a7db780956f14482419c4
Meng Feng <mfeng.pi@gmail.com>
2024-08-23 21:01:07 -0400

    refine pb sampler for dist, remove de_normalize

--- pud/envs/safe_pointenv/pb_sampler.py
+++ pud/envs/safe_pointenv/pb_sampler.py
@@ -6,7 +6,7 @@ from pud.algos.data_struct import arg_topk, get_nd_inds_set, arg_group_vals
 from pud.algos.lagrange.drl_ddpg_lag import DRLDDPGLag
 from pud.envs.safe_pointenv.safe_wrappers import \
     SafeGoalConditionedPointWrapper
-
+from pud.envs.safe_habitatenv.safe_habitat_wrappers import SafeGoalConditionedHabitatPointWrapper
 
 def calc_pairwise_cost(agent:DRLDDPGLag, rb_vec:np.ndarray, ensemble_agg="max"):
     pcost = agent.get_pairwise_cost(rb_vec, aggregate=None)
@@ -26,7 +26,87 @@ def calc_pairwise_dist(agent:DRLDDPGLag, rb_vec:np.ndarray, ensemble_agg="max"):
         pdist_agg = np.mean(pdist, axis=0)
     return pdist_agg
 
+
 def sample_pbs_by_agent(
+        env:Union[SafeGoalConditionedPointWrapper, SafeGoalConditionedHabitatPointWrapper], 
+        agent:DRLDDPGLag, 
+        num_states:int=100,
+        min_dist: float = 0,
+        max_dist: float = 10,
+        target_val:float=None,
+        ensemble_agg:str="max",
+        use_uncertainty:bool=True, # boost samples of high uncertainty 
+        uncertainty_lb:float=0.0, 
+        uncertainty_ub:float=1.0, 
+        K:int=5, # num of samples nearest to the target metric
+    ) -> List[dict]:
+    """sample problems with target metrics according to the predictions of the agent
+
+    Args:
+        env (SafeGoalConditionedPointWrapper): env that contains reset_orig, which returns normalized start-goal pairs
+        agent (DRLDDPGLag): 
+        pval_f: (agent, rb_vec) -> pairwise values
+        num_states (int, optional): _description_. Defaults to 100.
+    """
+    ## online generate start and goal states nearest to target cumulative costs
+    env: SafeGoalConditionedHabitatPointWrapper
+    rb_vec = np.array([env.sample_empty_state() for _ in range(num_states)])
+
+    rb_vec_goal = np.array([env.sample_empty_state() for _ in range(num_states)])
+
+    ## predict the pairwise costs
+    pdist = agent.get_pairwise_dist(
+            obs_vec=np.stack([env.normalize_obs(s) for s in rb_vec]), 
+            goal_vec=np.stack([env.normalize_obs(s) for s in rb_vec_goal]), 
+            aggregate=None,
+            )
+    pdist_agg = None
+    if ensemble_agg == "max":
+        pdist_agg = np.max(pdist, axis=0)
+    elif ensemble_agg == "mean":
+        pdist_agg = np.mean(pdist, axis=0)
+
+    pdist_std, pdist_std_mean = 0.0, 0.0
+    if use_uncertainty:
+        pdist_std = np.std(pdist, axis=0)
+        pdist_std_mean = np.mean(pdist_std)
+
+    lb_mask = pdist_agg + pdist_std >= min_dist
+    ub_mask = pdist_agg - pdist_std <= max_dist
+    prod_mask = lb_mask * ub_mask
+
+    gInds = np.where(prod_mask)
+    if len(gInds[0]) == 0:
+        return []
+    else:
+        pdist_gInds = pdist_agg[gInds]
+        scoring = 0.0 # smaller -> better
+        if target_val is not None:
+            scoring = scoring + np.abs(pdist_gInds - target_val)
+        # encourage diverse samples
+        if use_uncertainty: 
+            pdist_stds_gInds = pdist_std[gInds]
+            scoring = scoring - np.clip(pdist_stds_gInds,
+                    a_min=uncertainty_lb, a_max=uncertainty_ub)
+
+        K = min(K, len(scoring))
+        mInds = arg_topk(-scoring, topK=K) # find K minimum entries
+        gmInds = (gInds[0][mInds], gInds[1][mInds])
+
+        nearest_pbs = [None] * K
+        for n in range(K):
+            i,j = gmInds[0][n], gmInds[1][n]
+            nearest_pbs[n] = {
+                "start": rb_vec[i],
+                "goal": rb_vec_goal[j],
+                "info": {"prediction": pdist_agg[i,j],
+                        "proj_dist": pdist_agg[i,j],
+                        "ensemble_std_mean": pdist_std_mean,
+                        },
+                    }
+    return nearest_pbs
+
+def sample_pbs_by_agent_deprecated(
         env:SafeGoalConditionedPointWrapper, 
         agent:DRLDDPGLag, 
         num_states:int=100,
