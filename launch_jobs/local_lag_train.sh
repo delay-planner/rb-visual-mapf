5193651039286df2b86be142be7eee2c811c8b37
Viraj Parimi <parimiviraj@gmail.com>
2024-09-08 17:56:46 -0400

    added launch scripts for fourrooms modified

--- launch_jobs/local_lag_train.sh
+++ launch_jobs/local_lag_train.sh
@@ -1,14 +1,17 @@
 # !/bin/sh
 
-#device="cpu"
-device="cuda:1"
+device="cuda:0"
+#device="cuda:1"
 
 cd "${project_root}"
 
-ckpt="runs_debug/results/CenterDot/job_530956.centerdot/2024-07-30-16-52-28/ckpt/ckpt_0293000"
-config="runs_debug/results/CenterDot/job_530956.centerdot/2024-07-30-16-52-28/bk/bk_config.yaml"
+#ckpt="runs_debug/results/CenterDot/job_530956.centerdot/2024-07-30-16-52-28/ckpt/ckpt_0293000"
+ckpt="runs_debug/results/FourRoomsModified/job_/2024-08-29-17-00-49/ckpt/ckpt_0300000"
+#config="runs_debug/results/CenterDot/job_530956.centerdot/2024-07-30-16-52-28/bk/bk_config.yaml"
+config=runs_debug/results/FourRoomsModified/job_/2024-08-29-17-00-49/bk/bk_config.yaml
 lambda_lr=10
-illustration_pb_file="pud/envs/safe_pointenv/illustration_set/CenterDot_resize_3_linear_r3.txt"
+#illustration_pb_file="pud/envs/safe_pointenv/illustration_set/CenterDot_resize_3_linear_r3.txt"
+illustration_pb_file="pud/envs/safe_pointenv/illustration_set/FourRoomsModified_resize_5_linear_r3_4pts.txt"
 cost_limit=10.0
 
 num_iterations=600000
